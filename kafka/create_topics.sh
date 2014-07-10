2604d23bd72fe21e71b02bf7a5d75b40c2afef2a
Tim Kuhlman <tim.kuhlman@hp.com>
2014-07-10 12:48:35 -0600

    Initial kafka container

---
+++ kafka/create_topics.sh
@@ -0,0 +1,5 @@
+/opt/kafka/bin/kafka-server-start.sh /etc/kafka/server.properties
+/opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic metrics
+/opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic events
+/opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic alarm-state-transitions
+/opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic alarm-notifications

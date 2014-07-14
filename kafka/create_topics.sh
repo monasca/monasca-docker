af54892e9dadd5f4ef345f9ccb55f120dc27ece9
Tim Kuhlman <tim.kuhlman@hp.com>
2014-07-14 15:21:59 -0600

    Fix the create topics script to avoid exiting

--- kafka/create_topics.sh
+++ kafka/create_topics.sh
@@ -1,5 +1,7 @@
+set -m
 /opt/kafka/bin/kafka-server-start.sh /etc/kafka/server.properties &
 /opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic metrics
 /opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic events
 /opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic alarm-state-transitions
 /opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic alarm-notifications
+fg

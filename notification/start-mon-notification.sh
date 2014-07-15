e24cb013fd7d11a8eda4b4791be343bdfe15eb56
Tim Kuhlman <tim.kuhlman@hp.com>
2014-07-15 09:32:41 -0600

    Modified mon-notification startup to wait for kafka.

---
+++ notification/start-mon-notification.sh
@@ -0,0 +1,11 @@
+# First postfix needs to be running, the image is set to use local postfix
+/etc/init.d/postfix start
+
+# If kafka just started often the notification engine starts before it is ready, in which case retry
+start=`date "+%s"`
+/usr/local/bin/mon-notification
+duration=$[ `date "+%s"` - $start ]
+if [ $duration -lt 60 ]; then
+  sleep 60
+  /usr/local/bin/mon-notification
+fi

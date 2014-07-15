a64e35366d5d7421960e10cb5455a155a1c358fe
Tim Kuhlman <tim.kuhlman@hp.com>
2014-07-15 09:00:46 -0600

    Smarter topic creation

--- kafka/create_topics.sh
+++ kafka/create_topics.sh
@@ -1,7 +1,27 @@
+# Start up kafka, make topics and the put kafka back into the foreground
+# This could be done in a smarter way in particular the topic creation but this works and was quick to implement
+
 set -m
 /opt/kafka/bin/kafka-server-start.sh /etc/kafka/server.properties &
-/opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic metrics
-/opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic events
-/opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic alarm-state-transitions
-/opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic alarm-notifications
+
+`/opt/kafka/bin/kafka-list-topic.sh --zookeeper zookeeper:2181 | grep metrics`
+if [ $? -ne 0 ]; then
+  /opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic metrics
+fi
+
+`/opt/kafka/bin/kafka-list-topic.sh --zookeeper zookeeper:2181 | grep events`
+if [ $? -ne 0 ]; then
+  /opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic events
+fi
+
+`/opt/kafka/bin/kafka-list-topic.sh --zookeeper zookeeper:2181 | grep alarm-state-transitions`
+if [ $? -ne 0 ]; then
+  /opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic alarm-state-transitions
+fi
+
+`/opt/kafka/bin/kafka-list-topic.sh --zookeeper zookeeper:2181 | grep alarm-notifications`
+if [ $? -ne 0 ]; then
+  /opt/kafka/bin/kafka-create-topic.sh --zookeeper zookeeper:2181 --replica 1 --partition 4 --topic alarm-notifications
+fi
+
 fg

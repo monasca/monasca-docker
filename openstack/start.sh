e63c27ddc789adc740fe6b418337395ca165b09c
Tim Kuhlman <tim.kuhlman@hp.com>
2015-02-12 17:07:37 -0700

    Added add-hosts to the setup details.

--- openstack/start.sh
+++ openstack/start.sh
@@ -2,4 +2,4 @@
 keystone-manage --config-file=/etc/keystone/keystone.conf db_sync
 keystone-all --config-file /etc/keystone/keystone.conf
 /etc/init.d/apache2 start
-echo "The monasca api is configured to be at the hostname api, use container linking to set this."
+echo "The monasca api is configured to be at the hostname api, use container linking or --add-host to set this."

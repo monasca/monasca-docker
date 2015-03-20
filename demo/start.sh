26cf8c7547ddec4d4c76bd0dd5f15e3522193316
Tim Kuhlman <tim.kuhlman@hp.com>
2015-03-20 17:11:14 -0600

    Various fixes for the initial demo image.

--- demo/start.sh
+++ demo/start.sh
@@ -14,7 +14,7 @@ nova-api &
 # influxdb
 /usr/bin/influxdb -config=/opt/influxdb/shared/config.toml &
 
-#zookeeper
+# zookeeper
 . /etc/zookeeper/conf/environment
 [ -d $ZOO_LOG_DIR ] || mkdir -p $ZOO_LOG_DIR
 chown $USER:$GROUP $ZOO_LOG_DIR
@@ -22,14 +22,14 @@ chown $USER:$GROUP $ZOO_LOG_DIR
 if [ -z "$JMXDISABLE" ]; then
     JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.local.only=$JMXLOCALONLY"
 fi
-java -cp $CLASSPATH $JAVA_OPTS -Dzookeeper.log.dir=${ZOO_LOG_DIR} -Dzookeeper.root.logger=${ZOO_LOG4J_PROP} $ZOOMAIN $ZOOCFG
+java -cp $CLASSPATH $JAVA_OPTS -Dzookeeper.log.dir=${ZOO_LOG_DIR} -Dzookeeper.root.logger=${ZOO_LOG4J_PROP} $ZOOMAIN $ZOOCFG &
 
 # kafka and topic creation
 /opt/kafka/bin/kafka-server-start.sh /etc/kafka/server.properties &
 ansible-playbook -i /setup/hosts /setup/topics.yml -c local
 
 # Start the persister
-/usr/bin/java -Dfile.encoding=UTF-8 -Xmx8g -cp /opt/monasca/monasca-persister.jar:/opt/monasca/vertica/vertica_jdbc.jar monasca.persister.PersisterApplication server /etc/monasca/persister-config.yml
+/usr/bin/java -Dfile.encoding=UTF-8 -Xmx8g -cp /opt/monasca/monasca-persister.jar:/opt/monasca/vertica/vertica_jdbc.jar monasca.persister.PersisterApplication server /etc/monasca/persister-config.yml &
 
 # start the api
 /usr/bin/java -Xmx8g -cp /opt/monasca/monasca-api.jar monasca.api.MonApiApplication server /etc/monasca/api-config.yml &
@@ -46,6 +46,9 @@ sleep 60  # Takes a moment for storm to be up
 # notification engine
 /etc/init.d/postfix start
 /usr/local/bin/monasca-notification &
+
+# Setup alarms
+ansible-playbook -i /setup/hosts /setup/alarms.yml -c local
 
 # Finally tail the api log as we need to have something running in the foreground
 tail -f /var/log/monasca/api

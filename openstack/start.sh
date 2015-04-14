5281b7dda49b7e477f82afdd0a42e6e7636f5406
Tim Kuhlman <tim.kuhlman@hp.com>
2015-04-14 08:45:50 -0600

    Minor fix on the user notes

--- openstack/start.sh
+++ openstack/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "The keystone default admin token is 'ADMIN', admin/admin is user/pass. The monasca users still need to be setup."
+echo "The keystone default admin token is 'ADMIN', admin/admin/admin is user/pass/project. The monasca users still need to be setup."
 keystone-all --config-file /etc/keystone/keystone.conf &
 /etc/init.d/apache2 start
 nova-api

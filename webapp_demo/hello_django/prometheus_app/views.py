fd1c7599f79cf90e5237b73af5007a9afa6708e8
Kaiyan Sheng <kaiyan.sheng@hp.com>
2016-12-02 10:51:02 -0700

    Change button size and add labels in Counter

--- webapp_demo/hello_django/prometheus_app/views.py
+++ webapp_demo/hello_django/prometheus_app/views.py
@@ -4,7 +4,9 @@ from django.views.generic import TemplateView
 
 from prometheus_client import start_http_server, Counter
 
-CLICK_TIME = Counter('count_click_time', 'Description of counter')
+CLICK_TIME = Counter('button_click_counter', 'Counter for a button',
+                     ['version', 'button_name'])
+CLICK_TIME.labels(version='v1.0', button_name='START')
 
 # Start up the server to expose the metrics.
 start_http_server(8005)
@@ -17,10 +19,11 @@ class HomePageView(TemplateView):
                 'current_value': 0.0
             }
         if request.GET.get('click_btn'):
-            CLICK_TIME.inc(amount=1)
-            current_value = CLICK_TIME.__dict__.get('_value').__dict__.values()[0]
+            CLICK_TIME.labels(version='v1.0', button_name='START').inc(
+                amount=1)
+            current_value = CLICK_TIME.__dict__.get('_metrics').values()[
+                0].__dict__.get('_value').__dict__.values()[0]
             context = {
                 'current_value': current_value
             }
-            # print value
         return render(request, 'index.html', context=context)

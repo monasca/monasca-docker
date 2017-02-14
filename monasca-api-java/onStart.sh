7d22583ac7fe7ae2e69d9f357749cca210f799fa
Tim Buckley <timothy.jas.buckley@hpe.com>
2017-02-14 10:47:52 -0700

    Remove Dockerfiles for the Java API and persister

--- monasca-api-java/onStart.sh
+++
@@ -1,4 +0,0 @@
-#! bin/bash
-API_MEMORY_MAX=${API_MEMORY_MAX:-1024m}
-
-java -Dfile.encoding=UTF-8 -Xmx${API_MEMORY_MAX} -cp /monasca-api.jar monasca.api.MonApiApplication server /config/api-config.yml
--- monasca-persister-java/onStart.sh
+++
@@ -1,4 +0,0 @@
-#! bin/bash
-PERSISTER_MEMORY_MAX=${PERSISTER_MEMORY_MAX:-1024m}
-
-java -Dfile.encoding=UTF-8 -Xmx${PERSISTER_MEMORY_MAX} -cp /monasca-persister.jar monasca.persister.PersisterApplication server /config/persister-config.yml

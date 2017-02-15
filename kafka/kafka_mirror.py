018bd22576f694c59722aefe16c4b67438f3c63f
Tim Buckley <timothy.jas.buckley@hpe.com>
2017-02-15 12:00:39 -0700

    Rewrite kafka dockerfile

---
+++ kafka/kafka_mirror.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# (C) Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from __future__ import print_function
+
+import os
+import sys
+
+import requests
+
+MIRROR = os.environ.get('MIRROR', 'https://www.apache.org/dyn/closer.cgi')
+DIRECT = os.environ.get('DIRECT', None)
+PATH = '{mirror}kafka/{kafka}/kafka_{scala}-{kafka}.tgz'
+
+
+def main():
+    if len(sys.argv) != 3:
+        print('Usage: {0} [kafka_version] [scala_version]'.format(sys.argv[0]))
+        sys.exit(1)
+
+    kafka_version = sys.argv[1]
+    scala_version = sys.argv[2]
+    mirror = MIRROR
+
+    if not DIRECT:
+        r = requests.get(MIRROR, params={
+            'path': 'kafka_{0}-{1}.tgz'.format(scala_version, kafka_version),
+            'as_json': '1'
+        })
+        r.raise_for_status()
+        mirror = r.json()['preferred']
+
+    print(PATH.format(mirror=mirror, kafka=kafka_version, scala=scala_version))
+
+if __name__ == '__main__':
+    main()
--- kafka/start.sh
+++ kafka/start.sh
@@ -1,45 +1,87 @@
-#!/bin/bash -x
+#!/bin/ash
 
-cat /kafka/config/server.properties.template | sed \
-  -e "s|{{ZOOKEEPER_CONNECTION_STRING}}|${ZOOKEEPER_CONNECTION_STRING}|g" \
-  -e "s|{{ZOOKEEPER_CHROOT}}|${ZOOKEEPER_CHROOT:-}|g" \
-  -e "s|{{KAFKA_BROKER_ID}}|${KAFKA_BROKER_ID:-0}|g" \
-  -e "s|{{KAFKA_ADVERTISED_HOST_NAME}}|${KAFKA_ADVERTISED_HOST_NAME:-$IP}|g" \
-  -e "s|{{KAFKA_PORT}}|${KAFKA_LISTEN_PORT:-9092}|g" \
-  -e "s|{{KAFKA_ADVERTISED_PORT}}|${KAFKA_ADVERTISED_PORT:-9092}|g" \
-  -e "s|{{KAFKA_DELETE_TOPIC_ENABLE}}|${KAFKA_DELETE_TOPIC_ENABLE:-false}|g" \
-  -e "s|{{ZOOKEEPER_CONNECTION_TIMEOUT_MS}}|${ZOOKEEPER_CONNECTION_TIMEOUT_MS:-10000}|g" \
-  -e "s|{{ZOOKEEPER_SESSION_TIMEOUT_MS}}|${ZOOKEEPER_SESSION_TIMEOUT_MS:-10000}|g" \
-  -e "s|{{GROUP_MAX_SESSION_TIMEOUT_MS}}|${GROUP_MAX_SESSION_TIMEOUT_MS:-300000}|g" \
-   > /kafka/config/server.properties
+set -x
 
-# Kafka's built-in start scripts set the first three system properties here, but
-# we add two more to make remote JMX easier/possible to access in a Docker
-# environment:
-#
-#   1. RMI port - pinning this makes the JVM use a stable one instead of
-#      selecting random high ports each time it starts up.
-#   2. RMI hostname - normally set automatically by heuristics that may have
-#      hard-to-predict results across environments.
-#
-# These allow saner configuration for firewalls, EC2 security groups, Docker
-# hosts running in a VM with Docker Machine, etc. See:
-#
-# https://issues.apache.org/jira/browse/CASSANDRA-7087
-if [ -z $KAFKA_JMX_OPTS ]; then
+CONFIG_TEMPLATES="/templates"
+CONFIG_DEST="/kafka/config"
+
+KAFKA_LISTEN_PORT=${KAFKA_LISTEN_PORT:-"9092"}
+ZOOKEEPER_CONNECTION_STRING=${ZOOKEEPER_CONNECTION_STRING:-"zookeeper:2181"}
+export KAFKA_LISTEN_PORT ZOOKEEPER_CONNECTION_STRING
+
+ZOOKEEPER_WAIT=${ZOOKEEPER_WAIT:-"true"}
+ZOOKEEPER_WAIT_TIMEOUT=${ZOOKEEPER_WAIT_TIMEOUT:-"3"}
+ZOOKEEPER_WAIT_DELAY=${ZOOKEEPER_WAIT_DELAY:-"10"}
+ZOOKEEPER_WAIT_RETRIES=${ZOOKEEPER_WAIT_RETRIES:-"20"}
+
+first_zk=$(echo $ZOOKEEPER_CONNECTION_STRING | cut -d, -f1)
+zk_host=$(echo $first_zk | cut -d\: -f1)
+zk_port=$(echo $first_zk | cut -d\: -f2)
+
+# wait for zookeeper to become available
+if [ "$ZOOKEEPER_WAIT" = "true" ]; then
+  success="false"
+  for i in $(seq $ZOOKEEPER_WAIT_RETRIES); do
+    ok=$(echo ruok | nc $zk_host $zk_port -w $ZOOKEEPER_WAIT_TIMEOUT)
+    if [ $? -eq 0 -a "$ok" = "imok" ]; then
+      success="true"
+      break
+    else
+      echo "Connect attempt $i of $ZOOKEEPER_WAIT_RETRIES failed, retrying..."
+      sleep $ZOOKEEPER_WAIT_DELAY
+    fi
+  done
+
+  if [ "$success" != "true" ]; then
+    echo "Could not connect to $first_zk after $i attempts, exiting..."
+    sleep 1
+    exit 1
+  fi
+fi
+
+if [ "$KAFKA_HOSTNAME_FROM_IP" = "true" ]; then
+  # see also: http://stackoverflow.com/a/21336679
+  ip=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
+  echo "Using autodetected IP as advertised hostname: $ip"
+  export KAFKA_ADVERTISED_HOST_NAME=$ip
+fi
+
+# copy all normal .properties files
+for f in $CONFIG_TEMPLATES/*.properties; do
+  if [ -e "$f" ]; then
+    cp "$f" "$CONFIG_DEST/$(basename "$f")"
+  fi
+done
+
+# apply all *.properties.j2 templates
+for f in $CONFIG_TEMPLATES/*.properties.j2; do
+  if [ -e "$f" ]; then
+    python /template.py "$f" "$CONFIG_DEST/$(basename "$f" .j2)"
+  fi
+done
+
+if [ "$KAFKA_JMX" = "true" ]; then
+  KAFKA_JMX_PORT=${KAFKA_JMX_PORT:-"7203"}
+
+  # see also: https://github.com/ches/docker-kafka/blob/master/Dockerfile
+  # we don't export any kafka options by default so JVMs spawned on this
+  # container don't always hit port conflicts
+  # this should make it possible to e.g. run scripts in /kafka/bin/ on the
+  # runnign server without the JVM crashing due to the RMI port being used
+  export JMX_PORT=$KAFKA_JMX_PORT
+
+  if [ -z "$KAFKA_JMX_OPTS" ]; then
     KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote=true"
     KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
     KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.ssl=false"
-    KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT"
-    KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Djava.rmi.server.hostname=${JAVA_RMI_SERVER_HOSTNAME:-$KAFKA_ADVERTISED_HOST_NAME} "
+    KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.rmi.port=$KAFKA_JMX_PORT"
+    KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Djava.rmi.server.hostname=$KAFKA_ADVERTISED_HOST_NAME "
     export KAFKA_JMX_OPTS
+  fi
 fi
 
-function finish {
-  cat /kafka/config/server.properties
-  cat /kafka/logs/server.log
-}
-trap finish EXIT
+echo "Creating topics..."
+/create-topics.sh &
 
-echo "Starting kafka"
-/kafka/bin/kafka-server-start.sh /kafka/config/server.properties
+echo "Starting kafka..."
+exec /kafka/bin/kafka-server-start.sh "$CONFIG_DEST/server.properties"

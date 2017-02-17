ce008d417bd42892ddb24ceff25e1f553bc88f29
Tim Buckley <timothy.jas.buckley@hpe.com>
2017-02-17 16:06:34 -0700

    Improve retry logic in keystone container init

--- keystone/start.sh
+++ keystone/start.sh
@@ -6,14 +6,28 @@ if [[ "$KEYSTONE_DATABASE_BACKEND" =  "mysql" ]]; then
     mysql_user=${KEYSTONE_MYSQL_USER:-"keystone"}
     mysql_pass=${KEYSTONE_MYSQL_PASSWORD:-"keystone"}
     mysql_db=${KEYSTONE_MYSQL_DATABASE:-"keystone"}
+    retries=${KEYSTONE_MYSQL_CONNECT_RETRIES:-"24"}
+    delay=${KEYSTONE_MYSQL_CONNECT_RETRY_DELAY:-"5"}
 
     echo "Waiting for mysql to become available..."
-    mysqladmin ping \
-        --host="$mysql_host" \
-        --user="$mysql_user" \
-        --password="$mysql_pass" \
-        --wait=5
-    if [[ $? -ne 0 ]]; then
+    success="false"
+    for i in $(seq $retries); do
+        mysqladmin status \
+            --host="$mysql_host" \
+            --user="$mysql_user" \
+            --password="$mysql_pass" \
+            --connect_timeout=10
+        if [[ $? -eq 0 ]]; then
+            echo "MySQL is available, continuing..."
+            success="true"
+            break
+        else
+            echo "Connection attempt $i of $retries failed"
+            sleep "$delay"
+        fi
+    done
+
+    if [[ "$success" != "true" ]]; then
         echo "Unable to reach MySQL database! Exiting..."
         sleep 1
         exit 1

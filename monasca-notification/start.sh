ef05358e47524e3f0a2ab6abacebe8735c3ea45e
Tim Buckley <timothy.jas.buckley@hpe.com>
2017-02-17 18:01:49 -0700

    Refactor monasca-notification dockerfile

---
+++ monasca-notification/start.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+# (C) Copyright 2017 Hewlett Packard Enterprise Development LP
+
+MYSQL_WAIT_RETRIES=${MYSQL_WAIT_RETRIES:-"24"}
+MYSQL_WAIT_DELAY=${MYSQL_WAIT_DELAY:-"5"}
+
+KAFKA_WAIT_RETRIES=${KAFKA_WAIT_RETRIES:-"24"}
+KAFKA_WAIT_DELAY=${KAFKA_WAIT_DELAY:-"5"}
+
+echo "Waiting for MySQL to become available..."
+success="false"
+for i in $(seq $MYSQL_WAIT_RETRIES); do
+  mysqladmin status \
+      --host="$MYSQL_HOST" \
+      --port="$MYSQL_PORT" \
+      --user="$MYSQL_USERNAME" \
+      --password="$MYSQL_PASSWORD" \
+      --connect_timeout=10
+  if [ $? -eq 0 ]; then
+    echo "MySQL is available, continuing..."
+    success="true"
+    break
+  else
+    echo "Connection attempt $i of $MYSQL_WAIT_RETRIES failed"
+    sleep "$MYSQL_WAIT_DELAY"
+  fi
+done
+
+if [ "$success" != "true" ]; then
+    echo "Unable to reach MySQL database! Exiting..."
+    sleep 1
+    exit 1
+fi
+
+if [ -n "$KAFKA_WAIT_FOR_TOPICS" ]; then
+  echo "Waiting for Kafka topics to become available..."
+  success="false"
+
+  for i in $(seq $KAFKA_WAIT_RETRIES); do
+    python /kafka_wait_for_topics.py
+    if [ $? -eq 0 ]; then
+      success="true"
+      break
+    else
+      echo "Kafka not yet ready (attempt $i of $KAFKA_WAIT_RETRIES)"
+      sleep "$KAFKA_WAIT_DELAY"
+    fi
+  done
+fi
+
+if [ "$success" != "true" ]; then
+  echo "Kafka failed to become ready, exiting..."
+  sleep 1
+  exit 1
+fi
+
+echo "Starting notification engine..."
+if [ "$CONFIG_TEMPLATE" = "true" ]; then
+  python /template.py /config/notification.yaml.j2 /config/notification.yaml
+else
+  cp /config/notification.yaml.j2 /config/notification.yaml
+fi
+
+monasca-notification /config/notification.yaml

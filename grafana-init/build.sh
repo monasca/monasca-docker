55df03ab7b4fd4476a7f584cd1941c736ea85568
Tim Buckley <timothy.jas.buckley@hpe.com>
2017-03-02 14:36:24 -0700

    Bumb grafana-init version for new dashboards

--- grafana-init/build.sh
+++ grafana-init/build.sh
@@ -11,7 +11,7 @@ source "$(dirname "$(dirname "$(readlink -f $0)")")/docker_build_util.sh"
 # shellcheck disable=SC2034
 docker_repo=${1:-"monasca/grafana-init"}
 
-build 1.0.0
-retag 1.0.0 1.0
-retag 1.0.0 1
-retag 1.0.0 latest
+build 1.1.0
+retag 1.1.0 1.1
+retag 1.1.0 1
+retag 1.1.0 latest

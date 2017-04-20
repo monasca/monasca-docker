13af4124108576b62356cdc090fa26913b4f0def
Tim Buckley <timothy.jas.buckley@hpe.com>
2017-04-20 11:53:58 -0600

    Add basic upgrade support to mysql-init
    This add very preliminary 'upgrade' support to mysql init. mysql-init
    now automatically creates a schema version table and checks versions
    during init. Eventually it will use this table to apply upgrades
    sequentially based on the current installed schema version.
    
    Note that actual upgrade logic will come in a future PR. This patch
    is mainly intended to make mysql-init return successfully when run
    against an existing database. If an existing version is found it will
    print the version but will not process the `mysql-upgrades.d`
    directory yet.

--- mysql-init/init.sh
+++ mysql-init/init.sh
@@ -7,11 +7,13 @@ MYSQL_INIT_HOST=${MYSQL_INIT_HOST:-"mysql"}
 MYSQL_INIT_PORT=${MYSQL_INIT_PORT:-"3306"}
 MYSQL_INIT_USERNAME=${MYSQL_INIT_USERNAME:-"root"}
 MYSQL_INIT_PASSWORD=${MYSQL_INIT_PASSWORD:-"secretmysql"}
+MYSQL_INIT_SCHEMA_DATABASE=${MYSQL_INIT_DATABASE:-"mysql_init_schema"}
 
 MYSQL_INIT_WAIT_RETRIES=${MYSQL_INIT_WAIT_RETRIES:-"24"}
 MYSQL_INIT_WAIT_DELAY=${MYSQL_INIT_WAIT_DELAY:-"5"}
 
 USER_SCRIPTS="/mysql-init.d"
+UPGRADE_SCRIPTS="/mysql-upgrade.d"
 
 echo "Waiting for MySQL to become available..."
 success="false"
@@ -38,56 +40,73 @@ if [ "$success" != "true" ]; then
     exit 1
 fi
 
-set -e
+query="select major, minor, patch from schema_version order by id desc limit 1;"
+version=$(echo "$query" | mysql \
+    --host="$MYSQL_INIT_HOST" \
+    --user="$MYSQL_INIT_USERNAME" \
+    --port=$MYSQL_INIT_PORT \
+    --password="$MYSQL_INIT_PASSWORD" \
+    --silent \
+    $MYSQL_INIT_SCHEMA_DATABASE)
+if [ $? -eq 0 ]; then
+  echo "MySQL has already been initialized! Current version: $version"
 
-for f in $USER_SCRIPTS/*.sql.j2; do
-  if [ -e "$f" ]; then
-    echo "Applying template: $f"
-    python /template.py "$f" "$USER_SCRIPTS/$(basename "$f" .j2)"
+  # TODO apply upgrades
+  echo "Updating not yet implemented!"
+else
+  echo "MySQL has not yet been initialized. Initial schemas will be applied."
+
+  set -e
+
+  for f in $USER_SCRIPTS/*.sql.j2; do
+    if [ -e "$f" ]; then
+      echo "Applying template: $f"
+      python /template.py "$f" "$USER_SCRIPTS/$(basename "$f" .j2)"
+    fi
+  done
+
+  for f in $USER_SCRIPTS/*.sql; do
+    if [ -e "$f" ]; then
+      echo "Running script: $f"
+      mysql --host="$MYSQL_INIT_HOST" \
+          --user="$MYSQL_INIT_USERNAME" \
+          --port=$MYSQL_INIT_PORT \
+          --password="$MYSQL_INIT_PASSWORD" < "$f"
+    fi
+  done
+
+  if [ -n "$MYSQL_INIT_SET_PASSWORD" ]; then
+    echo "Updating password for $MYSQL_INIT_USERNAME..."
+
+    set +x
+    mysqladmin password \
+        --host="$MYSQL_INIT_HOST" \
+        --port=$MYSQL_INIT_PORT \
+        --user="$MYSQL_INIT_USERNAME" \
+        --password="$MYSQL_INIT_PASSWORD" \
+        "$MYSQL_INIT_SET_PASSWORD"
+  elif [ "$MYSQL_INIT_RANDOM_PASSWORD" = "true" ]; then
+    echo "Resetting $MYSQL_INIT_USERNAME password..."
+
+    set +x
+    pw=$(pwgen -1 32)
+    mysqladmin password \
+        --host="$MYSQL_INIT_HOST" \
+        --port=$MYSQL_INIT_PORT \
+        --user="$MYSQL_INIT_USERNAME" \
+        --password="$MYSQL_INIT_PASSWORD" \
+        "$pw"
+    echo "GENERATED $MYSQL_INIT_USERNAME PASSWORD: $pw"
+    MYSQL_INIT_PASSWORD="$pw"
   fi
-done
 
-for f in $USER_SCRIPTS/*.sql; do
-  if [ -e "$f" ]; then
-    echo "Running script: $f"
+  if [ "$MYSQL_INIT_DISABLE_REMOTE_ROOT" = "true" ]; then
+    echo "Disabling remote root login..."
     mysql --host="$MYSQL_INIT_HOST" \
         --user="$MYSQL_INIT_USERNAME" \
         --port=$MYSQL_INIT_PORT \
-        --password="$MYSQL_INIT_PASSWORD" < "$f"
+        --password="$MYSQL_INIT_PASSWORD" < /disable-remote-root.sql
   fi
-done
-
-if [ -n "$MYSQL_INIT_SET_PASSWORD" ]; then
-  echo "Updating password for $MYSQL_INIT_USERNAME..."
-
-  set +x
-  mysqladmin password \
-      --host="$MYSQL_INIT_HOST" \
-      --port=$MYSQL_INIT_PORT \
-      --user="$MYSQL_INIT_USERNAME" \
-      --password="$MYSQL_INIT_PASSWORD" \
-      "$MYSQL_INIT_SET_PASSWORD"
-elif [ "$MYSQL_INIT_RANDOM_PASSWORD" = "true" ]; then
-  echo "Resetting $MYSQL_INIT_USERNAME password..."
-
-  set +x
-  pw=$(pwgen -1 32)
-  mysqladmin password \
-      --host="$MYSQL_INIT_HOST" \
-      --port=$MYSQL_INIT_PORT \
-      --user="$MYSQL_INIT_USERNAME" \
-      --password="$MYSQL_INIT_PASSWORD" \
-      "$pw"
-  echo "GENERATED $MYSQL_INIT_USERNAME PASSWORD: $pw"
-  MYSQL_INIT_PASSWORD="$pw"
-fi
-
-if [ "$MYSQL_INIT_DISABLE_REMOTE_ROOT" = "true" ]; then
-  echo "Disabling remote root login..."
-  mysql --host="$MYSQL_INIT_HOST" \
-      --user="$MYSQL_INIT_USERNAME" \
-      --port=$MYSQL_INIT_PORT \
-      --password="$MYSQL_INIT_PASSWORD" < /disable-remote-root.sql
 fi
 
 echo "mysql-init exiting successfully"

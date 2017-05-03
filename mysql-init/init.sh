036a558f1f5343e2e09b9d1d8fe429645f0ffe3b
Tim Buckley <timothy.jas.buckley@hpe.com>
2017-05-03 15:18:15 -0600

    Add upgrade support to mysql-init
    This adds support for basic schema upgrades in mysql-init. When an
    existing schema is detected, mysql-init will now run (in order) all
    schemas in `/mysql-upgrade.d/{major}.{minor}.{patch}/*.sql{,.j2}` when
    when each version component is greater than the last-installed version
    but less than or equal to the SCHEMA_XYZ_REV variables defined in the
    Dockerfile.
    
    This means that to relese a new schema:
     * The 'clean slate' /mysql-init.d/ should be updated to include any
       necessary changes
     * A new version directory should be added in `/mysql-upgrade.d/x.y.z`.
       This directory should contain differential changes needed to update
       the previous version to this new version.
    
    Note that upgrade `.sql` scripts can also use j2 formatting when named
    `.sql.j2`.

--- mysql-init/init.sh
+++ mysql-init/init.sh
@@ -15,45 +15,34 @@ MYSQL_INIT_WAIT_DELAY=${MYSQL_INIT_WAIT_DELAY:-"5"}
 USER_SCRIPTS="/mysql-init.d"
 UPGRADE_SCRIPTS="/mysql-upgrade.d"
 
-echo "Waiting for MySQL to become available..."
-success="false"
-for i in $(seq $MYSQL_INIT_WAIT_RETRIES); do
-  mysqladmin status \
-      --host="$MYSQL_INIT_HOST" \
-      --port=$MYSQL_INIT_PORT \
-      --user="$MYSQL_INIT_USERNAME" \
-      --password="$MYSQL_INIT_PASSWORD" \
-      --connect_timeout=10
-  if [ $? -eq 0 ]; then
-    echo "MySQL is available, continuing..."
-    success="true"
-    break
-  else
-    echo "Connection attempt $i of $MYSQL_INIT_WAIT_RETRIES failed"
-    sleep "$MYSQL_INIT_WAIT_DELAY"
+wait_mysql() {
+  echo "Waiting for MySQL to become available..."
+  success="false"
+  for i in $(seq $MYSQL_INIT_WAIT_RETRIES); do
+    mysqladmin status \
+        --host="$MYSQL_INIT_HOST" \
+        --port=$MYSQL_INIT_PORT \
+        --user="$MYSQL_INIT_USERNAME" \
+        --password="$MYSQL_INIT_PASSWORD" \
+        --connect_timeout=10
+    if [ $? -eq 0 ]; then
+      echo "MySQL is available, continuing..."
+      success="true"
+      break
+    else
+      echo "Connection attempt $i of $MYSQL_INIT_WAIT_RETRIES failed"
+      sleep "$MYSQL_INIT_WAIT_DELAY"
+    fi
+  done
+
+  if [ "$success" != "true" ]; then
+      echo "Unable to reach MySQL database! Exiting..."
+      sleep 1
+      exit 1
   fi
-done
+}
 
-if [ "$success" != "true" ]; then
-    echo "Unable to reach MySQL database! Exiting..."
-    sleep 1
-    exit 1
-fi
-
-query="select major, minor, patch from schema_version order by id desc limit 1;"
-version=$(echo "$query" | mysql \
-    --host="$MYSQL_INIT_HOST" \
-    --user="$MYSQL_INIT_USERNAME" \
-    --port=$MYSQL_INIT_PORT \
-    --password="$MYSQL_INIT_PASSWORD" \
-    --silent \
-    $MYSQL_INIT_SCHEMA_DATABASE)
-if [ $? -eq 0 ]; then
-  echo "MySQL has already been initialized! Current version: $version"
-
-  # TODO apply upgrades
-  echo "Updating not yet implemented!"
-else
+clean_install() {
   echo "MySQL has not yet been initialized. Initial schemas will be applied."
 
   set -e
@@ -107,6 +96,118 @@ else
         --port=$MYSQL_INIT_PORT \
         --password="$MYSQL_INIT_PASSWORD" < /disable-remote-root.sql
   fi
+}
+
+schema_upgrade() {
+  version="$1"
+
+  # ash doesn't support arrays, this seems to be the most concise way to get
+  # fields by index
+  set $version
+  if [ "$#" -ne "3" ]; then
+    echo "Invalid version: '$version'"
+    sleep 1
+    exit 1
+  fi
+  c_major=$1
+  c_minor=$2
+  c_patch=$3
+  echo "Upgrading MySQL, current version: major=$c_major minor=$c_minor patch=$c_patch"
+
+  any_applied="false"
+  last_major=$c_major
+  last_minor=$c_minor
+  last_patch=$c_patch
+  for diff_version in $(ls $UPGRADE_SCRIPTS | sort -V); do
+    if [ ! -d "$UPGRADE_SCRIPTS/$diff_version" ]; then
+      echo "Ignoring: $UPGRADE_SCRIPTS/$diff_version"
+      continue
+    fi
+
+    # we explicitly want to word-split here...
+    # shellcheck disable=SC2046
+    set $(echo $diff_version | tr '.' ' ')
+    if [ "$#" -ne "3" ]; then
+      echo "Invalid version number in upgrade directory, quitting! $diff_version"
+      sleep 1
+      exit 1
+    fi
+
+    d_major=$1
+    d_minor=$2
+    d_patch=$3
+
+    if [ "$d_major" -le "$c_major" -a "$d_minor" -le "$c_minor" -a "$d_patch" -le "$c_patch" ]; then
+      echo "Skipping update $diff_version (too old)"
+      continue
+    fi
+
+    if [ "$d_major" -gt "$c_major" -a "$d_minor" -gt "$c_minor" -a "$d_patch" -gt "$c_patch" ]; then
+      echo "Warning: update too new: $diff_version. This update will not be applied!"
+      echo "Make sure to update SCHEMA_MAJOR_REV, SCHEMA_MINOR_REV, and SCHEMA_PATCH_REV!"
+      echo "No futher updates will be applied."
+      break
+    fi
+
+    echo "Applying update: $diff_version"
+
+    for f in $UPGRADE_SCRIPTS/$diff_version/*.sql.j2; do
+      if [ -e "$f" ]; then
+        echo "Applying template: $f"
+        python /template.py "$f" "$UPGRADE_SCRIPTS/$diff_version/$(basename "$f" .j2)"
+      fi
+    done
+
+    for f in $UPGRADE_SCRIPTS/$diff_version/*.sql; do
+      if [ -e "$f" ]; then
+        echo "Running script: $f"
+        set +x
+        mysql --host="$MYSQL_INIT_HOST" \
+            --user="$MYSQL_INIT_USERNAME" \
+            --port=$MYSQL_INIT_PORT \
+            --password="$MYSQL_INIT_PASSWORD" < "$f"
+        set -x
+        any_applied="true"
+      fi
+    done
+
+    last_major=$d_major
+    last_minor=$d_minor
+    last_patch=$d_patch
+  done
+
+  if [ "$any_applied" = "true" ]; then
+    database=${MYSQL_INIT_SCHEMA_DATABASE:-"mysql_init_schema"}
+    echo "Recording version in $database: $last_major.$last_minor.$last_patch"
+    query="insert into schema_version (major, minor, patch) values ($last_major, $last_minor, $last_patch);"
+    set +x
+    echo $query | mysql \
+        --host="$MYSQL_INIT_HOST" \
+        --user="$MYSQL_INIT_USERNAME" \
+        --port=$MYSQL_INIT_PORT \
+        --password="$MYSQL_INIT_PASSWORD" \
+        "$database"
+    set -x
+    echo "Finished applying updates."
+  else
+    echo "No updates to apply!"
+  fi
+
+
+}
+
+query="select major, minor, patch from schema_version order by id desc limit 1;"
+version=$(echo "$query" | mysql \
+    --host="$MYSQL_INIT_HOST" \
+    --user="$MYSQL_INIT_USERNAME" \
+    --port=$MYSQL_INIT_PORT \
+    --password="$MYSQL_INIT_PASSWORD" \
+    --silent \
+    $MYSQL_INIT_SCHEMA_DATABASE)
+if [ $? -eq 0 ]; then
+  schema_upgrade "$version"
+else
+  clean_install
 fi
 
 echo "mysql-init exiting successfully"

e3c412f3abf6f91655566a7bc65b95add83cdf6b
Craig Bryant <craig.bryant@hpe.com>
2017-05-23 22:48:40 -0600

    Configure tempest properly
    The monasca-user was mistakenly being added to
    tempest_roles in tempest.conf. This caused the monasca-user role
    to be added to all users for the tempest tests causing the
    read only tests to fail because the read only user became
    read/write
    
    Make almost all parameters configurable.
    
    Add README.md
    
    Remove tempest-tests from docker-compose.yaml, but show how to
    add it in README.md

--- tempest-tests/start.sh
+++ tempest-tests/start.sh
@@ -6,13 +6,40 @@ set -x
 MONASCA_API_WAIT_RETRIES=${MONASCA_API_WAIT_RETRIES:-"24"}
 MONASCA_API_WAIT_DELAY=${MONASCA_API_WAIT_DELAY:-"5"}
 
+OS_USERNAME=${OS_USERNAME:-"mini-mon"}
+OS_PASSWORD=${OS_PASSWORD:-"password"}
+OS_TENANT_NAME=${OS_TENANT_NAME:-"mini-mon"}
+OS_DOMAIN_NAME=${OS_DOMAIN_NAME:-"Default"}
+ALT_USERNAME=${ALT_USERNAME:-"mini-mon"}
+ALT_PASSWORD=${ALT_PASSWORD:-"password"}
+ALT_TENANT_NAME=${ALT_TENANT_NAME:-"mini-mon"}
+AUTH_USE_SSL=${AUTH_USE_SSL:-"False"}
+KEYSTONE_SERVER=${KEYSTONE_SERVER:-"keystone"}
+KEYSTONE_PORT=${KEYSTONE_PORT:-"35357"}
+
+USE_DYNAMIC_CREDS=${USE_DYNAMIC_CREDS:-"True"}
+ADMIN_PROJECT_NAME=${ADMIN_PROJECT_NAME:-"mini-mon"}
+ADMIN_USERNAME=${ADMIN_USERNAME:-"mini-mon"}
+ADMIN_PASSWORD=${ADMIN_PASSWORD:-"password"}
+ADMIN_DOMAIN_NAME=${ADMIN_DOMAIN_NAME:-"Default"}
+
+if [ "$AUTH_USE_SSL" = "False" ]; then
+  AUTH_PROTOCOL="http"
+else
+  AUTH_PROTOCOL="https"
+fi
+
+AUTH_URI="${AUTH_PROTOCOL}://${KEYSTONE_SERVER}:${KEYSTONE_PORT}/v2.0/"
+AUTH_URI_V3="${AUTH_PROTOCOL}://${KEYSTONE_SERVER}:${KEYSTONE_PORT}/v3/"
+
 if [ "$MONASCA_WAIT_FOR_API" = "true" ]; then
-  # TODO Need the correct credentials for this to work
   echo "Waiting for Monasca API to become available..."
   success="false"
 
   for i in $(seq $MONASCA_API_WAIT_RETRIES); do
-    monasca alarm-definition-list --limit 1
+    monasca --os-user-domain-name "${OS_DOMAIN_NAME}" --os-project-name mini-mon \
+       --os-auth-url "${AUTH_URI_V3}" --os-username "${OS_USERNAME}" \
+       --os-password "${OS_PASSWORD}" alarm-list --limit 1
     if [ $? -eq 0 ]; then
       success="true"
       break
@@ -29,25 +56,17 @@ if [ "$MONASCA_WAIT_FOR_API" = "true" ]; then
   fi
 fi
 
-if [ "$CONFIG_TEMPLATE" = "true" ]; then
-  python template.py \
-    /etc/tempest/tempest.conf.j2 \
-    /etc/tempest/tempest.conf
-else
-  cp /etc/tempest/tempest.conf.j2 /etc/tempest/tempest.conf
-fi
+python template.py \
+  /etc/tempest/tempest.conf.j2 \
+  /etc/tempest/tempest.conf
 
+cd /monasca-api
 export OS_TEST_PATH=./monasca_tempest_tests/tests/api
 
-cd /monasca-api && \
-sed -e "s/'anotherrole'/'monasca-read-only-user'/" -i.sv monasca_tempest_tests/tests/api/base.py && \
+OSTESTR_REGEX=${OSTESTR_REGEX:-"monasca_tempest_tests"}
 testr init && \
-testr list-tests monasca_tempest_tests > monasca_tests  && \
-testr run --load-list=monasca_tests  && \
+ostestr --serial --regex "${OSTESTR_REGEX}"
 
-FAILED=$?
-
-if [ $FAILED -ne 0]; then
-  sleep 3600
+if [ "$STAY_ALIVE" = "true" ]; then
+  sleep 7200
 fi
-exit $FAILED

4b3dfacbff4b2c6cf464f0aba45b7dadcdbb17be
mhoppal <michael.jam.hoppal@hpe.com>
2017-06-21 08:50:03 -0600

    Add bigger timeout

--- ci.py
+++ ci.py
@@ -389,7 +389,7 @@ def main():
     if p.wait() != 0:
         print('build failed, exiting!')
         sys.exit(p.returncode)
-    time.sleep(90)
+    time.sleep(180)
     run_smoke_tests()
 
     files = get_changed_files()

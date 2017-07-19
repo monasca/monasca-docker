54ad6a9662f9e1e5353e9425cd105ff50c4d0498
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2017-07-19 07:53:07 +0200

    Provide build.yml instead build.sh for log-api
    Following commit completes several items:
    
    * added build.yml to integrate into existing CI
    * removed all images, apart from master and stable/ocata
    * made constraints control a little bit easier with just
      contstrains branch variable

--- monasca-log-api/build.sh
+++ monasca-log-api/build.sh
@@ -8,25 +8,20 @@ set -e
 source "$(dirname "$(dirname "$(readlink -f $0)")")/docker_build_util.sh"
 
 # constraints
-CONSTRAINTS_BASE=https://git.openstack.org/cgit/openstack/requirements/plain/upper-constraints.txt
-CONSTRAINTS_OCATA=$CONSTRAINTS_BASE?h=stable/ocata
-CONSTRAINTS_MITAKA=$CONSTRAINTS_BASE?h=stable/mitaka
-CONSTRAINTS_NEWTON=$CONSTRAINTS_BASE?h=stable/newton
+CONSTRAINTS_OCATA=stable/ocata
 
 # $docker_repo is referenced in docker_build_util.sh
 # shellcheck disable=SC2034
-docker_repo=${1:-"kornicameister/monasca-log-api"}
+docker_repo=${1:-"kornicameister/log-api"}
 
 build master LOG_API_BRANCH=master
-
-build ocata LOG_API_BRANCH=stable/ocata CONSTRAINTS_FILE=$CONSTRAINTS_OCATA
-build newton LOG_API_BRANCH=stable/newton CONSTRAINTS_FILE=$CONSTRAINTS_NEWTON
-build mitaka LOG_API_BRANCH=stable/mitaka CONSTRAINTS_FILE=$CONSTRAINTS_MITAKA
+build ocata LOG_API_BRANCH=stable/ocata CONSTRAINTS_BRANCH=$CONSTRAINTS_OCATA
 
 # releases
+build 2.2.1 LOG_API_BRANCH=2.2.1
 build 2.1.0 LOG_API_BRANCH=2.1.0
 build 2.0.0 LOG_API_BRANCH=2.0.0
-build 1.4.1 LOG_API_BRANCH=1.4.1 CONSTRAINTS_FILE=$CONSTRAINTS_OCATA
+build 1.4.1 LOG_API_BRANCH=1.4.1 CONSTRAINTS_BRANCH=$CONSTRAINTS_OCATA
 
 # latest release
-retag 2.1.0 latest
+retag 2.2.1 latest

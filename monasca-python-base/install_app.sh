f2ad2b5c2ad06ff956009d476ee7fffd6382deaa
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2017-07-24 09:10:53 +0200

    Made installation possible.
    Consider this WIP, there is still some work that has to be done
    around argument processing.

--- monasca-python-base/install_app.sh
+++ monasca-python-base/install_app.sh
@@ -1,32 +1,50 @@
 #!/bin/sh
 
-REPO=$1
-BRANCH=$2
+set -x
+
+REPO="${1}"
+BRANCH="${2}"
 
 # list of extras i.e. monasca-agent['libvirt']
-EXTRAS=${3-""}
+EXTRAS="${3:-'-1'}"
 
 # extra dependencies not listed anywhere in project
 # neither in requirements not in setup.cfg extras
-EXTRA_DEPS=${4:-""}
+EXTRA_DEPS="${4:-'-1'}"
 
 # requirements file for given OpenStack release
-CONSTRAINTS_FILE=$5
+CONSTRAINTS_FILE="${5}"
 
-if [ -z $REPO and -z $BRANCH ]; then
-    echo "No repo and branch provided"
-fi
+function install() {
+    git init
+    git remote add origin $REPO
+    git fetch origin $BRANCH
+    git reset --hard FETCH_HEAD
 
-git init
-git remote add origin $REPO
-git fetch origin $BRANCH
-git reset --hard FETCH_HEAD
+    if [ $EXTRA_DEPS != -1 ]; then
+        pip install --no-cache-dir $EXTRA_DEPS -c $CONSTRAINTS_FILE
+    fi
+    if [ $EXTRA != -1 ]; then
+        for extra in $EXTRA[@]; do
+            pip install --no-cache-dir .[${extra}] -c $CONSTRAINTS_FILE
+        done
+    fi
 
-if [ -n $EXTRA_DEPS ]; then
-    pip install --no-cache-dir ${EXTRA_DEPS} -c $CONSTRAINTS_FILE
+    pip install --no-cache-dir -r requirements.txt -c $CONSTRAINTS_FILE
+    pip install --no-cache-dir . -c $CONSTRAINTS_FILE
+}
+
+function print_env() {
+    cat << EOF
+Build environment:
+PROJECT=${REPO}@${BRANCH}
+EXTRA=${EXTRA}
+CONSTRAINTS=${CONSTRAINTS_FILE}
+EXTRA_DEPENDENCIES=${EXTRA_DEPS}
+EOF
+}
+
+if [ -n "$REPO" ] && [ -n "$BRANCH" ]; then
+    print_env
+    install
 fi
-if [ -n $EXTRA ]; then
-    pip install --no-cache-dir .[${extra}] -c $CONSTRAINTS_FILE
-fi
-pip install --no-cache-dir -r requirements.txt -c $CONSTRAINTS_FILE
-python setup.py install

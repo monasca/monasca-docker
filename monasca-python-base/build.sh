f21938f6fe6dab79ae9f2510777e4baac8f0fb9d
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2017-07-25 11:54:07 +0200

    Fixed wrong director

--- monasca-python-base/build.sh
+++ monasca-python-base/build.sh
@@ -3,8 +3,8 @@
 apk add --no-cache python${PY_VER} py${PY_VER}-pip py${PY_VER}-jinja2 curl
 apk add --no-cache --virtual build-dep python${PY_VER}-dev git make g++ linux-headers
 
-sh /install.sh "$@"
-
-rm -rf /install.sh
+mkdir -p /app
+cd /app ; sh /install.sh "$@" ; cd -
+rm -rf /install.sh /app
 
 apk del build-dep

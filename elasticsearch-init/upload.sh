d13b38646bc0b634a34074026422ea42118f5454
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2017-07-31 13:58:04 +0200

    Get template name from the filename
    Instead of specyfying the name of the template
    manually, allow to get the TPL name from the mounted file.
    In the same time it allows to uplaod multiple templates
    at once.

---
+++ elasticsearch-init/upload.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+TPL_DIR=/templates
+
+_get_tpl_name_from_file() {
+    local tpl=$1
+    local tpl_name=$(basename $tpl)
+    echo $tpl_name
+}
+
+if [ ! -d $TPL_DIR ]; then
+    echo "No templates mounted"
+    exit 0
+else
+    TPLS=(ls $TPL_DIR)
+fi
+
+for template in $TPLS; do
+    echo "Handling template file $template"
+    tpl_name=`_get_tpl_name_from_file $template`
+
+    curl -XPUT --retry 2 --retry-delay 2 $ELASTICSEARCH_URI/_template/${tpl_name} -d @/$template
+
+done
+
+

d84a24e23e1cf3c620375bc6868331b3de593421
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2017-07-31 10:40:26 +0200

    Changed LOG_API_PORT variable name
    monasca-api variable for the port name starts with
    `MONASCA_CONTAINER_` prefix. Make equaivalent log-api
    variable to follow the same principle.

--- monasca-log-api/health-check.sh
+++ monasca-log-api/health-check.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Default values for executing it manually on the host
-LOG_API_PORT=${1:-5607}
+MONASCA_CONTAINER_LOG_API_PORT=${1:-5607}
 
 curl --include --silent --show-error --output - \
-    http://localhost:${LOG_API_PORT}/healthcheck 2>&1 | \
+    http://localhost:${MONASCA_CONTAINER_LOG_API_PORT}/healthcheck 2>&1 | \
     awk '
         BEGIN {status_code="0"; body=""; output=""}
         $1 ~ /^HTTP\// {status_line=$0; status_code=$2}

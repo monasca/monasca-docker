0eb7cf54942c72086b68da876bf9a5680f70d2b2
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2017-08-10 10:54:19 +0200

    Add missing Jinja2 installation

--- monasca-notification/apk_install.sh
+++ monasca-notification/apk_install.sh
@@ -1,4 +1,4 @@
 install_apk_deps() {
-    apk add --no-cache py2-jinja2 py2-netaddr py2-gevent py2-greenlet mysql-client ca-certificates
+    apk add --no-cache mysql-client ca-certificates
     apk add --no-cache --virtual build-dep git make g++ linux-headers
 }

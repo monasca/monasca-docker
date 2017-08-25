3083183f592c48fa84ca4c181c733ed29d8914cd
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2017-08-25 08:49:04 +0200

    Make it possible to change configuration
    The configuration file for kibana was not templated at all
    during the startup of the container. Thus the environmental
    variables were useless.
    
    This commit adds python and python2-jinja packages to
    enable templating similar to other monasca images.

---
+++ kibana/start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+python /template.py /kibana.yml.j2 /opt/kibana/config/kibana.yml
+
+if [ $MONASCA_PLUGIN_ENABLED == True ]; then
+    /wait-for.sh $KEYSTONE_URI -- kibana
+else
+    kibana
+fi

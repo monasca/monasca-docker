3f7321f0b343c336e6bb41dc71f8cf73cdd1b477
Andrea Adams <aadams@hpe.com>
2017-09-14 15:08:47 -0600

    Remove test junk

--- ci.py
+++ ci.py
@@ -183,7 +183,6 @@ def upload_manifest(pipeline, voting, uploaded_files, dirty_modules, files, tags
 def upload_files(log_dir, bucket):
     uploaded_files = {}
     blob = bucket.blob(log_dir)
-    print ('log_dir: {}'.format(log_dir))
     for f in os.listdir(log_dir):
         file_path = log_dir + f
         if os.path.isfile(file_path):

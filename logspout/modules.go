16b3a37c86255324247ce2ff5e91981840c175eb
Dobroslaw Zybort <dobroslaw.zybort@ts.fujitsu.com>
2017-09-20 14:17:26 +0200

    Add Logspout container

---
+++ logspout/modules.go
@@ -0,0 +1,7 @@
+package main
+
+import (
+	_ "github.com/gliderlabs/logspout/transports/tcp"
+	_ "github.com/gliderlabs/logspout/transports/udp"
+	_ "github.com/looplab/logspout-logstash"
+)
---
+++ logspout/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# (C) Copyright 2017 FUJITSU LIMITED
+
+/bin/logspout

cc6b8a1a08f290667e1139f46224423736c43a45
Dobroslaw Zybort <dobroslaw.zybort@ts.fujitsu.com>
2017-09-26 13:48:17 +0200

    Fix starting logspout

--- logspout/build.sh
+++ logspout/build.sh
@@ -9,6 +9,3 @@ go get -v
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
 apk del go git mercurial build-base
 rm -rf /go /var/cache/apk/* /root/.glide
-
-# backwards compatibility
-ln -fs /tmp/docker.sock /var/run/docker.sock

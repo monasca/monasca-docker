a1895134335e3d48d7211d40dcee22f64e55a985
Monasca CI <aadams@hpe.com>
2017-09-28 14:37:48 -0600

    Make keystone variables configurable

--- monasca-agent-forwarder/start.sh
+++ monasca-agent-forwarder/start.sh
@@ -5,6 +5,15 @@ set -x
 
 AGENT_CONF="/etc/monasca/agent"
 
+if [ "$KEYSTONE_DEFAULTS_ENABLED" == "true" ]; then
+  export OS_AUTH_URL=${OS_AUTH_URL:-"http://keystone:35357/v3/"}
+  export OS_USERNAME=${OS_USERNAME:-"monasca-agent"}
+  export OS_PASSWORD=${OS_PASSWORD:-"password"}
+  export OS_USER_DOMAIN_NAME=${OS_USER_DOMAIN_NAME:-"Default"}
+  export OS_PROJECT_NAME=${OS_PROJECT_NAME:-"mini-mon"}
+  export OS_PROJECT_DOMAIN_NAME=${OS_PROJECT_DOMAIN_NAME:-"Default"}
+fi
+
 template () {
   if [ "$CONFIG_TEMPLATE" = "true" ]; then
     python /template.py "$1" "$2"

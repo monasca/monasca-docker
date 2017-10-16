43db2f44dc10692bcf3db88812fa1625abe9fc80
Monasca CI <aadams@hpe.com>
2017-10-16 12:29:32 -0600

    Remove version from keystone default

--- monasca-agent-forwarder/start.sh
+++ monasca-agent-forwarder/start.sh
@@ -6,7 +6,7 @@ set -x
 AGENT_CONF="/etc/monasca/agent"
 
 if [ "$KEYSTONE_DEFAULTS_ENABLED" == "true" ]; then
-  export OS_AUTH_URL=${OS_AUTH_URL:-"http://keystone:35357/v3/"}
+  export OS_AUTH_URL=${OS_AUTH_URL:-"http://keystone:35357"}
   export OS_USERNAME=${OS_USERNAME:-"monasca-agent"}
   export OS_PASSWORD=${OS_PASSWORD:-"password"}
   export OS_USER_DOMAIN_NAME=${OS_USER_DOMAIN_NAME:-"Default"}
--- monasca-alarms/start.sh
+++ monasca-alarms/start.sh
@@ -5,7 +5,7 @@ MONASCA_API_WAIT_RETRIES=${MONASCA_API_WAIT_RETRIES:-"24"}
 MONASCA_API_WAIT_DELAY=${MONASCA_API_WAIT_DELAY:-"5"}
 
 if [ "$KEYSTONE_DEFAULTS_ENABLED" == "true" ]; then
-  export OS_AUTH_URL=${OS_AUTH_URL:-"http://keystone:35357/v3/"}
+  export OS_AUTH_URL=${OS_AUTH_URL:-"http://keystone:35357"}
   export OS_USERNAME=${OS_USERNAME:-"mini-mon"}
   export OS_PASSWORD=${OS_PASSWORD:-"password"}
   export OS_USER_DOMAIN_NAME=${OS_USER_DOMAIN_NAME:-"Default"}

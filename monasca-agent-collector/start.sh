c863634fd5cb52b1f95f90ed47ef9fc93f10b95d
Denis Poisson <denis.poisson@ts.fujitsu.com>
2017-10-20 13:00:04 +0200

    fixing another negative exit code check

--- monasca-agent-collector/start.sh
+++ monasca-agent-collector/start.sh
@@ -30,7 +30,7 @@ template () {
 }
 
 if [ "$HOSTNAME_FROM_KUBERNETES" = "true" ]; then
-  if AGENT_HOSTNAME=$(python /kubernetes_get_host.py); then
+  if ! AGENT_HOSTNAME=$(python /kubernetes_get_host.py); then
     echo "Error getting hostname from Kubernetes"
     return 1
   fi

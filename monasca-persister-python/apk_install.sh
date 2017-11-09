33ceec2480406b7d26a8f3cbd28073c34fba1c22
bandorf <matthias.bandorf@est.fujitsu.com>
2017-11-09 10:41:09 +0100

    monasca-persister:python:optimize apk_install.sh

--- monasca-persister-python/apk_install.sh
+++ monasca-persister-python/apk_install.sh
@@ -4,5 +4,5 @@ install_apk_deps() {
 #    apk add --no-cache curl mysql-client
 #    apk add --no-cache curl
 #    apk add --no-cache --virtual build-dep git make g++ linux-headers
-    apk add --no-cache --virtual build-dep git
+    apk add --no-cache --virtual build-dep
 }

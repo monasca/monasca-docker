55e56384ea198a4da63320e0c588c40ce6c91b0e
bandorf <matthias.bandorf@est.fujitsu.com>
2017-11-09 10:33:46 +0100

    monasca-persister-python: test for min. config. of apk_install.sh

--- monasca-persister-python/apk_install.sh
+++ monasca-persister-python/apk_install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 install_apk_deps() {
-    apk add --no-cache curl mysql-client
-    apk add --no-cache --virtual build-dep git make g++ linux-headers
+#    apk add --no-cache curl mysql-client
+#    apk add --no-cache curl
+#    apk add --no-cache --virtual build-dep git make g++ linux-headers
+    apk add --no-cache --virtual build-dep git
 }

b40812eb609fccf2cda284fee8c326643f6784fa
Brandstetter <christian.brandstetter@est.fujitsu.com>
2017-11-09 15:34:42 +0100

    logapi: fix apk handling

--- monasca-log-api/apk_install.sh
+++ monasca-log-api/apk_install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 install_apk_deps() {
-#    apk add --no-cache curl mysql-client
-#    apk add --no-cache curl
-    apk add --no-cache --virtual build-dep git make g++ linux-headers
-#    apk add --no-cache --virtual git
+    apk add --no-cache --virtual build-dep
 }

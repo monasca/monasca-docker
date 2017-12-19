56b05772ace237b9da34ba6423e7ba957e703a20
bandorf <matthias.bandorf@est.fujitsu.com>
2017-12-19 08:49:18 +0100

    Fix bug 13865: Too much log information for Grafana server: log-level (default) switched from info to warn

--- grafana/start.sh
+++ grafana/start.sh
@@ -4,5 +4,12 @@ if [ -n "$GF_DATABASE_PORT" ]; then
   export GF_DATABASE_HOST=$GF_DATABASE_HOST":"$GF_DATABASE_PORT
 fi
 
+export GRAFANA_LOG_LEVEL=${GRAFANA_LOG_LEVEL:-"warn"}
+
+FILENAME = /var/lib/grafana/public/dashboards/drilldown.js
+if [ ! -f $FILENAME ]; then
+   copy /drilldown.js /var/lib/grafana/public/dashboards/drilldown.js
+fi
+
 python /template.py /etc/grafana/grafana.ini.j2 /etc/grafana/grafana.ini
 /go/bin/grafana-server -config /etc/grafana/grafana.ini -homepath /var/lib/grafana

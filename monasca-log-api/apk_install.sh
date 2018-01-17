019ef6928df631a30af987c389449bd91b60fc56
bandorf <matthias.bandorf@est.fujitsu.com>
2018-01-17 12:21:13 +0100

    monasca-docker/monasca-log-api: apk_install.sh: install curl, so that docker healthcheck will be executed

--- monasca-log-api/apk_install.sh
+++ monasca-log-api/apk_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 install_apk_deps() {
-    apk add --no-cache --virtual build-dep git make g++ linux-headers
+    apk add --no-cache --virtual curl build-dep git make g++ linux-headers
 }

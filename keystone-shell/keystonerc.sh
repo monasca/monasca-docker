c86181b3e451642bbd9a59d40ced2b5e55d01599
Tim Buckley <timothy.jas.buckley@hpe.com>
2018-02-16 11:05:53 -0700

    Fix broken conditional in keystonerc.sh
    Signed-off-by: Tim Buckley <timothy.jas.buckley@hpe.com>

--- keystone-shell/keystonerc.sh
+++ keystone-shell/keystonerc.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 secret() {
+    if [ "$#" -ne 1 ]; then
+        echo "A secret name is required."
+        echo "Usage: secret <name>"
+        return 1
+    fi
+
     eval "$(python /keystone_shell_vars.py "$1")"
 }
 
 shell() {
-    if [ "$#" -ne 1 ]; then
+    if [ "$#" -eq 1 ]; then
         eval "$(python /keystone_shell_vars.py "$1")"
     fi
 

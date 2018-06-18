89f60b64f42e85163cf166597ba8aa26cd342b23
Lukasz Zajaczkowski <lukasz.zajaczkowski@ts.fujitsu.com>
2018-06-18 08:56:52 +0200

    Add monasca-statsd to CI

--- ci.py
+++ ci.py
@@ -55,7 +55,8 @@ METRIC_PIPELINE_MODULE_TO_COMPOSE_SERVICES = {
     'keystone': 'keystone',
     'monasca-alarms': 'alarms',
     'monasca-notification': 'monasca-notification',
-    'grafana-init': 'grafana-init'
+    'grafana-init': 'grafana-init',
+    'monsca-statsd': 'monasca-statsd'
 }
 LOGS_PIPELINE_MODULE_TO_COMPOSE_SERVICES = {
     'monasca-log-metrics': 'log-metrics',
